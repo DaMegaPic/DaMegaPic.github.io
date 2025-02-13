7c2df0c269ecbad324727a6bc5526bc12144d089
DaMegaPic <damegapic@gmail.com>
2025-02-13 12:00:26 -0500

    update

--- csce242/exercises/03flex/script.js
+++ csce242/exercises/03flex/script.js
@@ -9,7 +9,7 @@ document.getElementById("btn-color").onclick = () => {
     const messageP = document.getElementById("message-old");
     messageP.innerHTML = "Good bye";
     messageP.classList.toggle("sad");
-}
+};
 
 //Column 3
 document.getElementById("btn-happy").onclick = () => {
@@ -18,7 +18,7 @@ document.getElementById("btn-happy").onclick = () => {
     messageP.classList.remove("sad");
     messageP.classList.toggle("happy");
     document.getElementById("btn-clear").classList.remove("display-none");
-}
+};
 document.getElementById("btn-sad").onclick = () => {
     const messageP = document.getElementById("message");
     messageP.innerHTML = "Bad Times";
@@ -26,18 +26,18 @@ document.getElementById("btn-sad").onclick = () => {
     messageP.classList.toggle("sad");
     document.getElementById("btn-clear").classList.remove("display-none");
 
-}
+};
 document.getElementById("btn-clear").onclick = () => {
     const messageP = document.getElementById("message");
     messageP.innerHTML = "";
     messageP.classList.remove("happy");
     messageP.classList.remove("sad");
     document.getElementById("btn-clear").classList.add("display-none");
-}
+};
 document.getElementById("feeling").onkeyup = (event) => {
     document.getElementById("feeling-output").innerHTML = 
     "You are feeling " + event.currentTarget.value;
-}
+};
 document.getElementById("btn-choose-color").onclick = () => {
     const color = document.getElementById("txt-color").value.toLowerCase().trim();
     let mood = "Happy";
@@ -54,9 +54,26 @@ document.getElementById("btn-choose-color").onclick = () => {
 
     document.getElementById("color-message").innerHTML =
     `You chose ${color}. You are feeling ${mood}`;
-}
+};
 
 //toggling the nav
 document.getElementById("toggle-nav").onclick = () => {
     document.getElementById("nav-items").classList.toggle("hide-small");
-}
+};
+
+// btn-color-up-down
+
+let pos = 0;
+const changeCirclePos = (increment) => {
+    pos += increment;
+    document.getElementById("circle").style.setProperty("top", pos + "px");
+};
+
+document.getElementById("btn-down").onclick = () => {
+    changeCirclePos(-10);
+    console.log("down");
+};
+
+document.getElementById("btn-ups").onlick = () => {
+    changeCirclePos(10);
+};
--- projects/part3/script.js
+++ projects/part3/script.js
@@ -2,3 +2,5 @@ document.getElementById("toggle-nav").onclick = () => {
     document.getElementById("nav-items").classList.toggle("hide-small");
     console.log("Toggle Button Clicked")
 }
+
+
