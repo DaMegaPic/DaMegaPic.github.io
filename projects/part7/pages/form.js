6c7d491cbba6303dc8d56da1eb3dd6cf9f872ac0
unknown <damegapic@gmail.com>
2025-03-23 18:31:02 -0400

    part 7

---
+++ projects/part7/pages/form.js
@@ -0,0 +1,48 @@
+document.getElementById("request-block").onsubmit = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    const customerName = form.elements["name"].value;
+    const email = form.elements["email"].value;
+    const description = form.elements["description"].value;
+
+    console.log(`${customerName} at ${email} wants this done: ${description}`);
+};
+
+const sendEmail = async(json) => {
+    try {
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers:{
+                "Content-Type":"application/json",
+                Accept:"application/json"
+            },
+            body:json
+        });
+
+        return response;
+    } catch(error){
+        console.log(error);
+        result.innerHTML = "Sorry, your email couldn't be sent";
+    }
+};
+
+document.getElementById("contact-form").onsubmit = async(e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const formData = new FormData(form);
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+    const result = document.getElementById("result");
+    result.innerHTML = "Sending...";
+
+    const httpResult = await sendEmail(json);
+
+    if(httpResult.status == 200){
+        result.innerHTML = "Email successfully sent";
+    } else {
+        result.innerHTML = "Sorry, your email wasn't sent";
+    }
+}
---
+++ projects/part7/pages/script.js
@@ -0,0 +1,46 @@
+document.getElementById("toggle-nav").onclick = () => {
+    document.getElementById("nav-items").classList.toggle("hide-small");
+    console.log("Toggle Button Clicked")
+}
+
+if (window.location.pathname.includes("reviews.html")) {
+    const fetchReviews = async () => {
+        try {
+            const response = await fetch("https://damegapic.github.io/jsons/reviews.json");
+            const data = await response.json();
+            populateReviews(data.reviews);
+        }
+        catch (error) {
+            console.error("Error fetching reviews",error);
+        }
+    };
+
+    function populateReviews(reviews) {
+        const reviewsContainer = document.getElementById("reviews");
+        reviewsContainer.innerHTML = "";
+
+        reviews.forEach(review => {
+            const reviewElement = document.createElement("div");
+            reviewElement.classList.add("review");
+
+            reviewElement.innerHTML = `
+                <div class="review-header">
+                    <div class="review-header-text">
+                        <h5>${review.title}</h5>
+                        <h5>${"&#9733;".repeat(review.rating)}</h5>
+                    </div>
+                    <img src="${review.image}" alt="Review Image">
+                </div>
+                <p>${review.content}</p>
+                <div class="review-details">
+                    <p class="review-date">${review.date}</p>
+                    <p class="reviewer-name">${review.reviewer}</p>
+                </div>
+            `;
+
+            reviewsContainer.appendChild(reviewElement);
+        });
+    };
+
+    document.addEventListener("DOMContentLoaded", fetchReviews);
+}
